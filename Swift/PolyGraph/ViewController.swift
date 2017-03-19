a490981f0760be38c3eab5644c3dd1fb3a02ba4d
Bretton Wade <brettonw.wade@gmail.com>
2017-03-18 20:40:09 -0400

    setup for shifting to swift

---
+++ Swift/PolyGraph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PolyGraph
+//
+//  Created by Bretton Wade on 3/17/17.
+//  Copyright © 2017 Bretton Wade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
